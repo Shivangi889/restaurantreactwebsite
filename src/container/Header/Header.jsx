7910afbefff065f4b9bda452d71c360bddf35a9a
shivangi <106944056+codershiv09@users.noreply.github.com>
2022-10-18 10:31:18 +0530

    Update Header.jsx
    change

--- src/container/Header/Header.jsx
+++ src/container/Header/Header.jsx
@@ -11,7 +11,7 @@ const Header = () => {
 <div className='app__wrapper_info'>
 <SubHeading title = " Chase the new flavour"/>
 <h1>The Key to Fine Dining</h1>
-<p className='p__opensans' style={{margin:'2rem 0'}}>Sit tellus lobortis sed senectus vivamus molestie. Condimentum volutpat morbi facilisis quam scelerisque sapien. Et, penatibus aliquam amet tellus</p>
+<p className='p__opensans' style={{margin:'2rem 0'}}>Here’s the flagship of the Bengali restaurant that’s brought good traditional cooking to tables across the country. In my opinion, the fish fry here is the best you’ll find anywhere and the mochar (banana flower) chop is one of the most interesting items available on any Bengali menu.</p>
 <button type='button' className='custom__button'>Explore Menu</button>
 </div>
     <div className='app__wrapper_img'>
